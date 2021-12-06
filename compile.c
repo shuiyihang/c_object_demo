3709db334bbe1c472137e79e15d721f2073552cb
shuiyihang <hejunkuo@chipsguide.com>
2021-12-06 10:15:13 +0800

    父类指针指向子类对象，执行相同方法。更能体现动态多态的理念

--- compile.c
+++ compile.c
@@ -5,11 +5,11 @@
 int main()
 {
     pool_init();
-    struct Rect* rect=Rect_creat(5,5,10,10);//对象
-    rect->base.vptr->area(rect);//多态
+    struct shape* rect=(struct shape*)Rect_creat(5,5,10,10);//对象
+    rect->vptr->area(rect);//多态
 
-    struct Round* round=Round_creat(7,8,5);//对象
-    round->base.vptr->area(round);//多态
+    struct shape* round=(struct shape*)Round_creat(7,8,5);//对象
+    round->vptr->area(round);//多态
 
     free(rect);
     free(round);
